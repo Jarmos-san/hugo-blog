ee9549544bf420b31b186efa5902127a9867f085
Jarmos <Jarmos-san>
2020-09-09 16:41:21 +0530

    :tada: Fresh new start

---
+++ README.md
@@ -0,0 +1,9 @@
+# Personal Website
+
+This is my personal website, it's still Work-In-Progress so come back later to check up on any new updates.
+
+## Licensing Terms & Conditions
+
+The site was generated using [Pelican](https:www.getpelican.com) a Static Site Generator built using Python. So all source code found in this repository which is used for generating the site are licensed under the terms & conditions of the [original LICENSE available at the Pelican repository](https://github.com/getpelican/pelican/blob/master/LICENSE).
+
+Although, the literary contents available as "blog posts" on the website are licensed under [CC-BY-4.0](https://spdx.org/licenses/CC-BY-4.0.html) License. Refer to the [LICENSE](LICENSE) document for information.
---
+++ blog/pelicanconf.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*- #
+
+AUTHOR = 'Somraj Saha'
+SITENAME = "Jarmos's Blog"
+SITEURL = 'http://localhost:8000'
+
+PATH = 'content'
+
+TIMEZONE = 'Asia/Kolkata'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+THEME = '../pelican-themes/pelican-blue'
+
+# Social widget
+SOCIAL = (
+    ('linkedin', 'https://www.linkedin.com/in/jarmos'),
+    ('github', 'https://github.com/Jarmos-san'),
+    ('twitter', 'https://twitter.com/Jarmosan')
+)
+
+DEFAULT_PAGINATION = 6
+
+STATIC_PATHS = [
+    'static/favicon.ico',
+]
+
+EXTRA_PATH_METADATA = {
+    'static/favicon.ico': {'path': 'favicon.ico'},
+}
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+SIDEBAR_DIGEST = 'Machine Learning Engineer'
+
+DISPLAY_PAGES_ON_MENU = True
+
+# TWITTER_USERNAME = 'jarmosan'
+
+MENUITEMS = (
+    ('Blog', SITEURL),
+)
