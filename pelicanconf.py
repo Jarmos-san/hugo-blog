f0fabf3035cc6de3d9a81a30d245c535a50f0ba7
Jarmos-san <somraj.1994@gmail.com>
2020-10-02 13:19:10 +0530

    :wrench: Update config for new theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -12,22 +12,22 @@ TIMEZONE = "Asia/Kolkata"
 DEFAULT_LANG = "en"
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
-AUTHOR_FEED_ATOM = None
-AUTHOR_FEED_RSS = None
+FEED_ALL_ATOM = "feeds/all.atom.xml"
+# CATEGORY_FEED_ATOM = None
+# TRANSLATION_FEED_ATOM = None
+# AUTHOR_FEED_ATOM = None
+# AUTHOR_FEED_RSS = None
 
-THEME = "themes/bootstrap"
+THEME = "themes/blue-penguin"
 
 # Social widget
-SOCIAL = (
-    ("linkedin", "https://www.linkedin.com/in/jarmos"),
-    ("github", "https://github.com/Jarmos-san"),
-    ("twitter", "https://twitter.com/Jarmosan"),
-)
+# SOCIAL = (
+#     ("linkedin", "https://www.linkedin.com/in/jarmos"),
+#     ("github", "https://github.com/Jarmos-san"),
+#     ("twitter", "https://twitter.com/Jarmosan"),
+# )
 
-DEFAULT_PAGINATION = 6
+# DEFAULT_PAGINATION = 2
 
 STATIC_PATHS = [
     "static/favicon.ico",
@@ -40,8 +40,37 @@ EXTRA_PATH_METADATA = {
 # Uncomment following line if you want document-relative URLs when developing
 # RELATIVE_URLS = True
 
-SIDEBAR_DIGEST = "An ML Engineer's Thoughts & Learnings"
+# Optional & all blue-penguin specific settings
+# all the following settings are *optional*
 
-DISPLAY_PAGES_ON_MENU = False
+# HTML metadata
+SITEDESCRIPTION = "Somraj's Thoughts & Learning as a Machine Learning Engineer"
 
-# MENUITEMS = (("Blog", SITEURL),)
+# all defaults to True.
+DISPLAY_HEADER = True
+DISPLAY_FOOTER = True
+DISPLAY_HOME = True
+DISPLAY_MENU = True
+
+# provided as examples, they make ‘clean’ urls. used by MENU_INTERNAL_PAGES.
+TAGS_URL = "tags"
+TAGS_SAVE_AS = "tags/index.html"
+AUTHORS_URL = "authors"
+AUTHORS_SAVE_AS = "authors/index.html"
+CATEGORIES_URL = "categories"
+CATEGORIES_SAVE_AS = "categories/index.html"
+ARCHIVES_URL = "archives"
+ARCHIVES_SAVE_AS = "archives/index.html"
+
+# use those if you want pelican standard pages to appear in your menu
+MENU_INTERNAL_PAGES = (
+    # ("Tags", TAGS_URL, TAGS_SAVE_AS),
+    # ("Authors", AUTHORS_URL, AUTHORS_SAVE_AS),
+    ("Categories", CATEGORIES_URL, CATEGORIES_SAVE_AS),
+    ("Archives", ARCHIVES_URL, ARCHIVES_SAVE_AS),
+)
+# additional menu items
+# MENUITEMS = (
+#     ("GitHub", "https://github.com/"),
+#     ("Linux Kernel", "https://www.kernel.org/"),
+# )
