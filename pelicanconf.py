9bd421f2d64b200c9e619e9e5c5bde923c780a55
Jarmos-san <somraj.1994@gmail.com>
2020-10-21 10:31:47 +0530

    :wrench: Seperate dev & publish config files for easier maintenance

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,7 +3,8 @@
 
 AUTHOR = "Somraj Saha"
 SITENAME = "Jarmos's Blog"
-SITEURL = "https://jarmos.netlify.app"
+SITEURL = ""
+SITEDESCRIPTION = "Somraj's Thoughts & Learning as a Machine Learning Engineer"
 
 PATH = "content"
 
@@ -12,39 +13,18 @@ TIMEZONE = "Asia/Kolkata"
 DEFAULT_LANG = "en"
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = "feeds/all.atom.xml"
-# CATEGORY_FEED_ATOM = None
-# TRANSLATION_FEED_ATOM = None
-# AUTHOR_FEED_ATOM = None
-# AUTHOR_FEED_RSS = None
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
 
 THEME = "themes/blue-penguin"
-
-# Social widget
-# SOCIAL = (
-#     ("linkedin", "https://www.linkedin.com/in/jarmos"),
-#     ("github", "https://github.com/Jarmos-san"),
-#     ("twitter", "https://twitter.com/Jarmosan"),
-# )
-
-# DEFAULT_PAGINATION = 2
 
 STATIC_PATHS = [
     "static/favicon.ico",
 ]
 
-EXTRA_PATH_METADATA = {
-    "static/favicon.ico": {"path": "favicon.ico"},
-}
-
-# Uncomment following line if you want document-relative URLs when developing
-# RELATIVE_URLS = True
-
-# Optional & all blue-penguin specific settings
-# all the following settings are *optional*
-
-# HTML metadata
-SITEDESCRIPTION = "Somraj's Thoughts & Learning as a Machine Learning Engineer"
 
 # all defaults to True.
 DISPLAY_HEADER = True
@@ -65,12 +45,6 @@ ARCHIVES_SAVE_AS = "archives/index.html"
 # use those if you want pelican standard pages to appear in your menu
 MENU_INTERNAL_PAGES = (
     # ("Tags", TAGS_URL, TAGS_SAVE_AS),
-    # ("Authors", AUTHORS_URL, AUTHORS_SAVE_AS),
     ("Categories", CATEGORIES_URL, CATEGORIES_SAVE_AS),
     ("Archives", ARCHIVES_URL, ARCHIVES_SAVE_AS),
 )
-# additional menu items
-# MENUITEMS = (
-#     ("GitHub", "https://github.com/"),
-#     ("Linux Kernel", "https://www.kernel.org/"),
-# )
---
+++ publishconf.py
@@ -0,0 +1,5 @@
+import pelicanconf
+
+SITEURL = "https://jarmos.netlify.app"
+
+FEED_ALL_ATOM = "feeds/all.atom.xml"
